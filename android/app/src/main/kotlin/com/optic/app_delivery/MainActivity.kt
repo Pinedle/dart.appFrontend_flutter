ceee1f272c19413d625b73fb6574db5f9a2fa1bc
Pinedle <lleninesteban@gmail.com>
2024-07-08 12:11:11 -0500

    respository flutter app frontend

---
+++ android/app/src/main/kotlin/com/optic/app_delivery/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.optic.app_delivery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/src/environment/environment.dart
@@ -0,0 +1,11 @@
+class Environment {
+
+  static const String API_URL  = "http://192.168.0.5:3000/";
+  static const String API_MERCADO_PAGO  = "https://api.mercadopago.com/v1";
+  static const String API_URL_OLD  = "192.168.0.5:3000";
+  static const String API_KEY_MAPS  = "AIzaSyB02Scjfok1G5cMCuNtmT_qeBby0liw-6w";
+
+  static const String ACCESS_TOKEN  = "TEST-293952657900162-062616-abfca01aa3bc85a6d1719dc570c4ed77-404430069";
+  static const String PUBLIC_KEY  = "TEST-ffdaecce-5d79-456c-b986-af1e059b0ef0";
+
+}
